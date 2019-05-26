fbc668927760c45787cba481b775058bf4900ee4
Jacob Lam <jlam18@binghamton.edu>
2019-05-26 14:53:16 -0400

    made player 1 format same as player 2 and center circle

--- sumo_frontend/src/index.js
+++ sumo_frontend/src/index.js
@@ -12,5 +12,53 @@ document.addEventListener("DOMContentLoaded",function(){
     }else {
     }
   })
-  
+
+  document.addEventListener("keypress",function(e){
+
+    // const prevKey1 = pressKey
+  pressKey = e.key
+
+    if (enterplayers){
+    switch(pressKey){
+      //player 1 listener
+      case 'w':
+        dy1 += -1
+        break;
+      case 's':
+        dy1 += 1
+        break;
+      case 'a':
+        dx1 += -1
+        break;
+      case 'd':
+        dx1 += 1
+        break;
+      case ' ':
+        dx1 = 0
+        dy1 = 0
+        break;
+      // player 1 listener end
+
+      //player 2 listener
+      case 'i':
+        dy2 = -1
+        break;
+      case 'k':
+        dy2 = 1
+        break;
+      case 'j':
+        dx2 = -1
+        break;
+      case 'l':
+        dx2 = 1
+        break;
+      case ',':
+        dx2 = 0
+        dy2 = 0
+      break;
+        //player 2 listener end
+    }//switch end
+  }
+  })
+
 })
--- sumo_frontend/src/player.js
+++ sumo_frontend/src/player.js
@@ -2,10 +2,10 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var ballRadius = 20;
 //player 1
-var x = canvas.width/2 - 50 ;
-var y = canvas.height-250;
-var dx = 0;
-var dy = 0;
+var x1 = canvas.width/2 - 50 ;
+var y1 = canvas.height-250;
+var dx1 = 0;
+var dy1 = 0;
 var acc1 = .99
 //player 2
 var x2 = canvas.width/2 + 50 ;
@@ -14,68 +14,18 @@ var dx2 = 0;
 var dy2 = 0;
 var acc2 = .99
 
-var cx = canvas.width/2 ;
-var cy = canvas.height/2 ;
+var x3 = canvas.width/2 ;
+var y3 = canvas.height/2 ;
 var circRad = 300
 
 
 const distance = function(){
-  return Math.sqrt((x-x2)**2 +(y-y2)**2)
+  return Math.sqrt((x1-x2)**2 +(y1-y2)**2)
 }
-var pressKey = 0
-
-document.addEventListener("keypress",function(e){
-
-  const prevKey = pressKey
-  pressKey = e.key
-
-  if (enterplayers){
-  switch(pressKey){
-    //player 1 listener
-    case 'w':
-      dy += -1
-      break;
-    case 's':
-      dy += 1
-      break;
-    case 'a':
-      dx += -1
-      break;
-    case 'd':
-      dx += 1
-      break;
-    case ' ':
-      dx = 0
-      dy = 0
-      break;
-    // player 1 listener end
-
-    //player 2 listener
-    case 'i':
-      dy2 = -1
-      break;
-    case 'k':
-      dy2 = 1
-      break;
-    case 'j':
-      dx2 = -1
-      break;
-    case 'l':
-      dx2 = 1
-      break;
-      case ',':
-        dx2 = 0
-        dy2 = 0
-        break;
-      //player 2 listener end
-  }//switch end
-}
-})
-
 
   function player1() {
       ctx.beginPath();
-      ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+      ctx.arc(x1, y1, ballRadius, 0, Math.PI*2);
       ctx.fillStyle = "#0095DD";
       ctx.fill();
       ctx.closePath();
@@ -91,7 +41,7 @@ document.addEventListener("keypress",function(e){
 
   function deathCircle() {
     ctx.beginPath();
-    ctx.arc(cx, cy, circRad, 0, Math.PI *2);
+    ctx.arc(x3, y3, circRad, 0, Math.PI *2);
     ctx.stroke();
     ctx.closePath();
   }
@@ -104,11 +54,11 @@ function draw() {
     player2();
     deathCircle();
     //allows player 1 to bounce off walls
-    if(x + dx > (canvas.width-ballRadius) || x + dx < ballRadius) {
-        dx = -dx ;
+    if(x1 + dx1 > (canvas.width-ballRadius) || x1 + dx1 < ballRadius) {
+        dx1 = -dx1 ;
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-        dy = -dy;
+    if(y1 + dy1 > canvas.height-ballRadius || y1 + dy1 < ballRadius) {
+        dy1 = -dy;
     }
 
     //allows player 2 to bounce off walls
@@ -121,13 +71,13 @@ function draw() {
 
     // collision of players
     if(distance() <= 2 * ballRadius){
-      dx = -dx;
-      dy = -dy;
+      dx1 = -dx1
+      dy1 = -dy1
       dx2 = -dx2
       dy2 = -dy2
       //new location of player 1
-      x += dx;
-      y += dy;
+      x1 += dx1
+      y1 += dy1
       //new location of player 2
       x2 += dx2;
       y2 += dy2;
@@ -136,10 +86,10 @@ function draw() {
 
     }else{
       //new location of player 1
-      dx = dx * acc1
-      dy = dy * acc1
-      x += dx;
-      y += dy;
+      dx1 = dx1 * acc1
+      dy1 = dy1 * acc1
+      x1 += dx1;
+      y1 += dy1;
       //new location of player 2
       dx2 = dx2 * acc2
       dy2 = dy2 * acc2
