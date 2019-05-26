a769878c2830ff925f6f5d733f86524a8bc2198b
github username <github email address>
2019-05-26 18:19:48 -0400

    made changes

--- sumo_frontend/src/index.js
+++ sumo_frontend/src/index.js
@@ -1,16 +1,21 @@
 const  inputName1= document.getElementById("player1")
 const inputName2 = document.getElementById("player2")
 const container = document.getElementById("container")
-const form = document.getElementsByTagName('form')[0]
+const form = document.getElementById("username")
+const startBtn = document.getElementById('startBtn')
+const restartBtn = document.getElementById('restartBtn')
 // const jsfile = document.getElementsByClassName('jsfile')[0]
-
+// let startGame = false
 container.addEventListener('click', e=>{
-      if(e.target.className === 'start'){
-        if(inputName1.value && inputName2.value){
-          form.style.display = 'none'
-          e.target.innerText = 'Restart'
-      }else {
-        alert('Please create Username!')
-        }
-      }
+  if(e.target.id === 'startBtn'){
+    if(inputName1.value !== '' & inputName2.value !== ''){
+          startBtn.style.display = 'none';
+          myCanvas.style.display = 'block';
+          restartBtn.style.display = 'block';
+          form.style.display = 'none';
+       }else{
+         alert('Please enter username')
+       }
+  }
+
 })
