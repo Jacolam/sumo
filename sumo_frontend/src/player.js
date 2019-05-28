33a5dc95c3ce51fe89b9f92304bad0d2d8de4f17
Jacob Lam <jlam18@binghamton.edu>
2019-05-28 14:29:05 -0400

    working collosion

--- sumo_frontend/src/player.js
+++ sumo_frontend/src/player.js
@@ -7,20 +7,20 @@ var y1 = canvas.height-250;
 var dx1 = 0;
 var dy1 = 0;
 var acc1 = .99
+
 //player 2
 var x2 = canvas.width/2 + 50 ;
 var y2 = canvas.height-250;
 var dx2 = 0;
 var dy2 = 0;
 var acc2 = .99
-
+// circle params
 var x3 = canvas.width/2 ;
 var y3 = canvas.height/2 ;
 var circRad = 300
 
 var player1Points = 0
 var player2Points = 0
-
 
 const distance = function(x1, x2,y1, y2){
   return Math.sqrt((x1-x2)**2 +(y1-y2)**2)
@@ -56,6 +56,19 @@ function draw() {
     player1();
     player2();
     deathCircle();
+
+    if( Math.abs(dx1) < .2){
+      dx1 = 0
+    }
+    if (Math.abs(dy1) <.2){
+      dy1 = 0
+    }
+    if( Math.abs(dx2) < .2){
+      dx2 = 0
+    }
+    if (Math.abs(dy2) <.2){
+      dy2 = 0
+    }
     //allows player 1 to bounce off walls
     if(x1 + dx1 > (canvas.width-ballRadius) || x1 + dx1 < ballRadius) {
         dx1 = -dx1 ;
@@ -74,10 +87,19 @@ function draw() {
 
     // collision of players
     if(distance(x1, x2, y1, y2) <= 2 * ballRadius){
-      dx1 = -dx1
-      dy1 = -dy1
-      dx2 = -dx2
-      dy2 = -dy2
+      if(dx1=== 0 && dy1=== 0){
+        console.log("player 1 is not moving")
+        dx1 = -dx2
+        dy1 = -dy2
+      }else if (dx2=== 0 && dy2=== 0){
+        console.log("player 2 is not moving")
+        dx2 = -dx1
+        dy2 = -dy1
+      }else{
+      dx1 = -dx1 * 1.5
+      dy1 = -dy1  * 1.5
+      dx2 = -dx2 * 1.5
+      dy2 = -dy2 * 1.5
       //new location of player 1
       x1 += dx1
       y1 += dy1
@@ -85,7 +107,7 @@ function draw() {
       x2 += dx2;
       y2 += dy2;
 
-      circRad -= .05
+      circRad -= .05}
 
     }else if (distance(x1, x3 ,y1, y3) >= circRad){
       console.log("Point to player 2")
@@ -122,7 +144,5 @@ function draw() {
         circRad -= .05
       }// circle only begins to move after the game has started
     }
-
 }
-
 setInterval(draw, 10);
