93b7d9c698d17f03179128ed1405e6aec686dfe9
Jacob Lam <47907079+Jacolam@users.noreply.github.com>
2019-05-28 10:13:06 -0400

    Revert "User option"

--- sumo_frontend/src/index.js
+++ sumo_frontend/src/index.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-const  inputName1= document.getElementById("player1")
-const inputName2 = document.getElementById("player2")
-const container = document.getElementById("container")
-const form = document.getElementById("username")
-const startBtn = document.getElementById('startBtn')
-const restartBtn = document.getElementById('restartBtn')
-// const jsfile = document.getElementsByClassName('jsfile')[0]
-// let startGame = false
-container.addEventListener('click', e=>{
-  if(e.target.id === 'startBtn'){
-    if(inputName1.value !== '' & inputName2.value !== ''){
-          startBtn.style.display = 'none';
-          myCanvas.style.display = 'block';
-          restartBtn.style.display = 'block';
-          form.style.display = 'none';
-       }else{
-         alert('Please enter username')
-       }
-  }
-=======
 const startForm = document.querySelector("#startForm")
 let enterplayers = false
 
@@ -100,6 +79,5 @@ document.addEventListener("DOMContentLoaded",function(){
     }//switch end
   }
   })
->>>>>>> master
 
 })
