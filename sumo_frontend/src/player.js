ae023b4a5fb374dbbc43c127f77a732d96807ea1
Jacob Lam <jlam18@binghamton.edu>
2019-05-30 17:46:10 -0400

    fixed timer

--- sumo_frontend/src/player.js
+++ sumo_frontend/src/player.js
@@ -200,7 +200,7 @@ function draw() {
     roundtimer--
     let seconds = Math.round(roundtimer / 100)
 
-    if(seconds === 40){ /*ends the game*/
+    if(seconds === 0){ /*ends the game*/
       if (p1Points > p2Points){
 
         canvas.style.display = 'none'
